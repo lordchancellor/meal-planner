4299e7a94a27b180a6798d06d5574d4f8171c41e
lordchancellor <c.d.j.ford@gmail.com>
2018-02-17 10:22:43 +0000

    Mocked up app links in the HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mp-home',
   template: `
-    <mat-toolbar>Some links</mat-toolbar>
+    <mat-toolbar>
+      <a *ngFor="let link of links">{{ link }}</a>
+    </mat-toolbar>
     <h1>Home</h1>
   `,
-  styles: ['']
+  styles: [
+    'mat-toolbar { justify-content: flex-end; }',
+    'a { display: inline-block; margin: 0 5px; font-size: 1.2rem; font-weight: 400; }'
+  ]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  links: string[];
 
   constructor() {}
+
+  // Initialisation Methods
+  ngOnInit(): void {
+    this.links = [
+      'Manage Recipes',
+      'Plan Week',
+      'Shopping List',
+      'Settings'
+    ];
+  }
 
 }
